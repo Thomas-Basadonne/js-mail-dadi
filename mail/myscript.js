49a637e4285c0fc109dc816b9680de94765ed4c5
Thomas-Basadonne <thommybasa@gmail.com>
2023-01-26 17:38:21 +0100

    final commit email

--- mail/myscript.js
+++ mail/myscript.js
@@ -2,15 +2,29 @@
 let email = ["paolorossi@gmail.com", "marcoverdi@icloud.com", "ileniabianchi@libero.it", "giuliaesposito@gmail.com", "thommybasa@gmail.com"];
 
 // chiedo all'utente di inserire la sua mail con un imput 
-var emailUser = document.getElementById("inputEmail").value;
-console.log(emailUser);
+
 // un bottone per il controllo
 var element = document.getElementById("btnEmail");
 element.addEventListener("click",
     function(){
-       if(emailUser)
+        var emailUser = document.getElementById("inputEmail").value;
+        console.log(emailUser);
+       
+        //se la mail è presente nell'arry compare scritta "puoi accedere"
+            if (email.includes(emailUser)){
+                console.log("true");
+                document.getElementById("result").innerHTML ="Email accettata!";
+            } else { // se non è presente comapre "non puoi accedere"
+                console.log("false");
+                document.getElementById("result").innerHTML = "Email rifiutata!";
+            }
     }
 );
-//se la mail è presente nell'arry compare scritta "puoi accedere"
-// se non è presente comapre "non puoi accedere"
 // bunus aggiungere email
+var element = document.getElementById("btnAdd");
+element.addEventListener("click",
+    function () {
+        var emailUser = document.getElementById("inputEmail").value;
+        email.push(emailUser);
+    }
+);
