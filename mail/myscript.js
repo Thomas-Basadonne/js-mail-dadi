bfba35f293186f8b4a764655c9a8fb8d520fd8a3
Thomas-Basadonne <thommybasa@gmail.com>
2023-01-26 17:18:17 +0100

    commit

--- mail/myscript.js
+++ mail/myscript.js
@@ -2,13 +2,13 @@
 let email = ["paolorossi@gmail.com", "marcoverdi@icloud.com", "ileniabianchi@libero.it", "giuliaesposito@gmail.com", "thommybasa@gmail.com"];
 
 // chiedo all'utente di inserire la sua mail con un imput 
-var emailUser = document.getElementById("inputEmail");
+var emailUser = document.getElementById("inputEmail").value;
 console.log(emailUser);
 // un bottone per il controllo
 var element = document.getElementById("btnEmail");
 element.addEventListener("click",
     function(){
-        if(emailUser )
+       if(emailUser)
     }
 );
 //se la mail è presente nell'arry compare scritta "puoi accedere"
